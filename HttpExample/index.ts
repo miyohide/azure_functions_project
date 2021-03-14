02305135b2621c06d57687ac2a2523caedc2c4a7
Hidenori Miyoshi <miyohide@gmail.com>
2021-03-14 18:34:35 +0900

    delete log outputs

--- HttpExample/index.ts
+++ HttpExample/index.ts
@@ -2,8 +2,6 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    context.log.warn("これは警告ログです");
-    context.log.error("これはエラーログです");
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
