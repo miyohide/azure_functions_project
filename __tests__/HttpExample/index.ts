8bf68b7b9c1307e8131f35fa17fb9c49f0165899
Hidenori Miyoshi <miyohide@gmail.com>
2021-03-16 20:52:25 +0900

    add test for log/log.info

--- __tests__/HttpExample/index.ts
+++ __tests__/HttpExample/index.ts
@@ -3,8 +3,15 @@ import httpTrigger from "../../HttpExample/index";
 let context = null;
 
 beforeEach(() => {
+    // context.log("...")とcontext.log.info("...")のテストをしたい場合の参考
+    // see. https://stackoverflow.com/questions/61732004/mocking-different-log-levels-on-azure-function-context-with-jest-in-javascript
     context = {
-        log: jest.fn()
+        log: (function () {
+            let main = <any>jest.fn((m) => m);
+            let info = jest.fn((m) => m);
+            main.info = info;
+            return main;
+        })()
     }
 });
 
@@ -15,6 +22,9 @@ test("when request has name query string, return greeting message", async () => 
     await httpTrigger(context, request);
 
     expect(context.log.mock.calls.length).toBe(1);
+    expect(context.log.mock.calls[0][0]).toEqual("HTTP trigger function processed a request.");
+    expect(context.log.info.mock.calls.length).toBe(1);
+    expect(context.log.info.mock.calls[0][0]).toEqual("info log")
     expect(context.res.body).toEqual("Hello, Bill. This HTTP triggered function executed successfully.");
 });
 
@@ -23,5 +33,8 @@ test("when request has not name query string, return greeting message", async ()
     await httpTrigger(context, request);
 
     expect(context.log.mock.calls.length).toBe(1);
+    expect(context.log.mock.calls[0][0]).toEqual("HTTP trigger function processed a request.");
+    expect(context.log.info.mock.calls.length).toBe(1);
+    expect(context.log.info.mock.calls[0][0]).toEqual("info log")
     expect(context.res.body).toEqual("This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.");
 });
