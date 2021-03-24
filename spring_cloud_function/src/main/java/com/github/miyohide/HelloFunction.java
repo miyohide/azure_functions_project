ac567498c1aa69891e5e833c59f6199e4822c382
Hidenori Miyoshi <miyohide@gmail.com>
2021-03-24 21:01:23 +0900

    print log message in Function method.

--- spring_cloud_function/src/main/java/com/github/miyohide/HelloFunction.java
+++ spring_cloud_function/src/main/java/com/github/miyohide/HelloFunction.java
@@ -1,5 +1,7 @@
 package com.github.miyohide;
 
+import com.microsoft.azure.functions.ExecutionContext;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
@@ -7,9 +9,11 @@ import java.util.function.Function;
 // /api/helloで呼び出されるHTTPTriggerの処理の実装。
 // @FunctionNameで指定した値と合わせる必要がある。
 @Component("hello")
-public class HelloFunction implements Function<User, Greeting> {
+public class HelloFunction implements Function<Message<User>, Greeting> {
     @Override
-    public Greeting apply(User user) {
-        return new Greeting(user.getName() + "!!!");
+    public Greeting apply(Message<User> m) {
+        ExecutionContext context = m.getHeaders().get("executionContext", ExecutionContext.class);
+        context.getLogger().info("HelloFunction.apply method");
+        return new Greeting(m.getPayload().getName() + "!!!");
     }
 }
