7a7c8d43bf0eee0e65782eb4904ca86f1fd06482
Hidenori Miyoshi <miyohide@gmail.com>
2021-03-27 21:21:20 +0900

    create ExecutionContext implementation for test.

---
+++ spring_cloud_function/src/test/java/com/github/miyohide/TestExecutionContext.java
@@ -0,0 +1,29 @@
+package com.github.miyohide;
+
+import com.microsoft.azure.functions.ExecutionContext;
+
+import java.util.UUID;
+import java.util.logging.Logger;
+
+public class TestExecutionContext implements ExecutionContext {
+    private String name;
+
+    public TestExecutionContext(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Logger getLogger() {
+        return Logger.getLogger(TestExecutionContext.class.getName());
+    }
+
+    @Override
+    public String getInvocationId() {
+        return UUID.randomUUID().toString();
+    }
+
+    @Override
+    public String getFunctionName() {
+        return this.name;
+    }
+}
