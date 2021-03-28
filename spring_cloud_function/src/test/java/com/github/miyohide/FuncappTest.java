9757d22d4d6d7159d80796cd807e6675cbea5e49
Hidenori Miyoshi <miyohide@gmail.com>
2021-03-28 16:17:54 +0900

    delete unnecessary import.

--- spring_cloud_function/src/test/java/com/github/miyohide/FuncappTest.java
+++ spring_cloud_function/src/test/java/com/github/miyohide/FuncappTest.java
@@ -2,22 +2,15 @@ package com.github.miyohide;
 
 import com.github.miyohide.model.Greeting;
 import com.github.miyohide.model.User;
-import com.microsoft.azure.functions.HttpRequestMessage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.function.context.FunctionCatalog;
 import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Optional;
 import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
