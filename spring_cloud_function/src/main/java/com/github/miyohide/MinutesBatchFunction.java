a1588bccd2e9b855e68e7bbc54d5d8222b931b57
Hidenori Miyoshi <miyohide@gmail.com>
2021-03-28 21:00:08 +0900

    implement batch app.

--- spring_cloud_function/src/main/java/com/github/miyohide/MinutesBatchFunction.java
+++ spring_cloud_function/src/main/java/com/github/miyohide/MinutesBatchFunction.java
@@ -1,13 +1,18 @@
 package com.github.miyohide;
 
+import com.microsoft.azure.functions.ExecutionContext;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
 @Component("minutesBatch")
-public class MinutesBatchFunction implements Function<String, String> {
+public class MinutesBatchFunction implements Function<Message<String>, String> {
     @Override
-    public String apply(String s) {
-        return null;
+    public String apply(Message<String> m) {
+        ExecutionContext context = m.getHeaders().get("executionContext", ExecutionContext.class);
+        context.getLogger().info("start minutes batch");
+        context.getLogger().info("end minutes batch");
+        return "SUCCESS";
     }
 }
