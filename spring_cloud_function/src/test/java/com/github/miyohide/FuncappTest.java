3cd724c76dd5d075521361aeb3d8dd463ed3082a
Hidenori Miyoshi <miyohide@gmail.com>
2021-03-28 16:16:51 +0900

    Fix fail test.

--- spring_cloud_function/src/test/java/com/github/miyohide/FuncappTest.java
+++ spring_cloud_function/src/test/java/com/github/miyohide/FuncappTest.java
@@ -1,29 +1,42 @@
 package com.github.miyohide;
 
+import com.github.miyohide.model.Greeting;
+import com.github.miyohide.model.User;
+import com.microsoft.azure.functions.HttpRequestMessage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.cloud.function.context.FunctionCatalog;
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(classes = Funcapp.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@FunctionalSpringBootTest
 class FuncappTest {
     @Autowired
-    private TestRestTemplate rest;
+    private FunctionCatalog catalog;
 
     @Test
-    public void test() throws URISyntaxException {
-        final String sendMessage = "send message value";
-        ResponseEntity<String> result = this.rest.exchange(
-                RequestEntity.post(new URI("/hello")).body(sendMessage), String.class
-        );
-        assertEquals("{\"name\":\"Welcome, " + sendMessage + "\"}", result.getBody());
+    public void test() {
+        final String send_message = "foor bar";
+        Function<Message<User>, Greeting> func = catalog.lookup("hello");
+        Message<User> m = MessageBuilder.withPayload(new User(send_message))
+                .setHeader("executionContext", new TestExecutionContext("aaa"))
+                .build();
+        Greeting g = func.apply(m);
+        assertEquals(send_message + "!!!", g.getName());
     }
 
 }
