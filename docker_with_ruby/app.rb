728bb094ce81e08a0ba9b9adf2e3264588148cce
Hidenori Miyoshi <miyohide@gmail.com>
2021-04-04 18:00:41 +0900

    rewrite modular style

--- docker_with_ruby/app.rb
+++ docker_with_ruby/app.rb
@@ -1,8 +1,13 @@
-require 'sinatra'
+require 'sinatra/base'
 
-set :environment, :production
-set :port, ENV['FUNCTIONS_CUSTOMHANDLER_PORT']
+class MyApp < Sinatra::Base
+  configure do
+    set :environment, :production
+    set :port, ENV['FUNCTIONS_CUSTOMHANDLER_PORT']
+  end
+  get '/api/Hello' do
+    'Hello Ruby World'
+  end
 
-get '/api/Hello' do
-  'Hello Ruby World'
+  run! if app_file === $0
 end
