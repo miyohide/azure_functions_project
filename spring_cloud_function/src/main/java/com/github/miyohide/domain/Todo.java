ca6ae0ec63eebcabdf94545751b91417cb7d611e
Hidenori Miyoshi <miyohide@gmail.com>
2021-04-13 20:19:19 +0900

    create domain model.

---
+++ spring_cloud_function/src/main/java/com/github/miyohide/domain/Todo.java
@@ -0,0 +1,51 @@
+package com.github.miyohide.domain;
+
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+public class Todo {
+    @Id
+    private String id;
+    private String title;
+    private boolean finished;
+    private LocalDateTime createdAt;
+
+    public Todo(String title) {
+        this.title = title;
+        this.finished = false;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
