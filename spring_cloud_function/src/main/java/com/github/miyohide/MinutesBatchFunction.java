5f36e89dd6ac2e624ee99a5c436df3ff4d00f2f9
Hidenori Miyoshi <miyohide@gmail.com>
2021-04-13 20:48:40 +0900

    call TodoRepository CRUD methods.

--- spring_cloud_function/src/main/java/com/github/miyohide/MinutesBatchFunction.java
+++ spring_cloud_function/src/main/java/com/github/miyohide/MinutesBatchFunction.java
@@ -1,6 +1,8 @@
 package com.github.miyohide;
 
+import com.github.miyohide.repository.TodoRepository;
 import com.microsoft.azure.functions.ExecutionContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
@@ -8,10 +10,15 @@ import java.util.function.Function;
 
 @Component("minutesBatch")
 public class MinutesBatchFunction implements Function<Message<String>, String> {
+    @Autowired
+    TodoRepository todoRepository;
+
     @Override
     public String apply(Message<String> m) {
         ExecutionContext context = m.getHeaders().get("executionContext", ExecutionContext.class);
         context.getLogger().info("start minutes batch");
+        long c = todoRepository.count();
+        context.getLogger().info("count = [" + c + "]");
         context.getLogger().info("end minutes batch");
         return "SUCCESS";
     }
