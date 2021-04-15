a72978cbc4e250a62ae852576b86437065c306c0
Hidenori Miyoshi <miyohide@gmail.com>
2021-04-15 21:06:49 +0900

    Fix test

--- spring_cloud_function/src/test/java/com/github/miyohide/repository/TodoRepositoryTest.java
+++ spring_cloud_function/src/test/java/com/github/miyohide/repository/TodoRepositoryTest.java
@@ -4,19 +4,24 @@ import com.github.miyohide.domain.Todo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 @DataJdbcTest
+@AutoConfigureTestDatabase(replace = NONE)
 public class TodoRepositoryTest {
     @Autowired
     TodoRepository todoRepository;
 
     @Test
     void testCrudMethods() {
-        Todo t = new Todo("test todo");
-        assertEquals(0, todoRepository.count());
-        todoRepository.save(t);
+        String todo_title = "test todo";
+        Todo t = new Todo(todo_title);
         assertEquals(1, todoRepository.count());
+        Todo savedTodo = todoRepository.save(t);
+        assertEquals(2, todoRepository.count());
+        assertEquals(todo_title, savedTodo.getTitle());
     }
 }
