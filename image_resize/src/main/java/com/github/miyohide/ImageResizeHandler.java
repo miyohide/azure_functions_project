7725ec7ec4c888b36e7e3e4ffe2bc614953bdd87
Hidenori Miyoshi <miyohide@gmail.com>
2021-05-05 18:06:03 +0900

    add blob input

--- image_resize/src/main/java/com/github/miyohide/ImageResizeHandler.java
+++ image_resize/src/main/java/com/github/miyohide/ImageResizeHandler.java
@@ -2,21 +2,20 @@ package com.github.miyohide;
 
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.OutputBinding;
-import com.microsoft.azure.functions.annotation.BlobOutput;
-import com.microsoft.azure.functions.annotation.EventGridTrigger;
-import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.StorageAccount;
+import com.microsoft.azure.functions.annotation.*;
 import org.springframework.cloud.function.adapter.azure.FunctionInvoker;
 
 public class ImageResizeHandler extends FunctionInvoker<EventSchema, Boolean> {
     @FunctionName("ImageResize")
-    @StorageAccount("OutputStorage")
+    @StorageAccount("ImageStorage")
     public boolean imageResizeHandler(
             @EventGridTrigger(name = "event") EventSchema event,
+            @BlobInput(name = "file", dataType = "binary", path = "{data.url}") byte[] content,
             @BlobOutput(name = "target", path = "myblob/{DateTime}-sample.txt")OutputBinding<String> outputItem,
             final ExecutionContext context
             ) {
         context.getLogger().info("***** EventGrid Trigger Start *****");
+        context.getLogger().info("----- Blob input data size = [" + content.length + "] -----");
         handleRequest(event, context);
         context.getLogger().info("***** EventGrid Trigger End *****");
         return true;
