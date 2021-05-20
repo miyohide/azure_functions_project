ef6b8755641153fc81e2d4f98b674afc20983542
Hidenori Miyoshi <miyohide@gmail.com>
2021-05-20 20:36:27 +0900

    use handleRequest method

--- image_resize_with_queue/src/main/java/com/github/miyohide/FileUploadHandler.java
+++ image_resize_with_queue/src/main/java/com/github/miyohide/FileUploadHandler.java
@@ -7,25 +7,19 @@ import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.QueueOutput;
 import org.springframework.cloud.function.adapter.azure.FunctionInvoker;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Paths;
-
-public class FileUploadHandler extends FunctionInvoker<EventSchema, String> {
+public class FileUploadHandler extends FunctionInvoker<String, String> {
   @FunctionName("FileUploadHandler")
   public void pushToQueue(
       @EventGridTrigger(name = "event") EventSchema event,
       @QueueOutput(name = "queue", queueName = "myqueue", connection = "MyQueueConnection")
           OutputBinding<String> queue,
       final ExecutionContext context)
-      throws MalformedURLException {
-    try {
-      URL url = new URL(event.data.get("url").toString());
-      String filename = Paths.get(url.getPath()).getFileName().toString();
+      throws IllegalArgumentException {
+    String filename = handleRequest(event.data.get("url").toString(), context);
+    if (filename == null) {
+      throw new IllegalArgumentException("Event Grid data invalid");
+    } else {
       queue.setValue(filename);
-    } catch (MalformedURLException e) {
-      context.getLogger().severe(e.getLocalizedMessage());
-      throw e;
     }
   }
 }
